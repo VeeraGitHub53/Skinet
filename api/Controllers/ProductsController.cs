706b7b3802515a5964ec276db9da00884cd8a71a
VeeraGitHub53 <veerababubonthu123@gmail.com>
2024-03-18 12:51:04 +0530

    end of section 3

--- api/Controllers/ProductsController.cs
+++ api/Controllers/ProductsController.cs
@@ -19,29 +19,29 @@ namespace api.Controllers
           this.context = context;
         }
 
-       [HttpGet("GetProducts")]
+       [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
           var products=await context.Products.ToListAsync();          
           return products ;
         }
 
-        //[Cached(600)]
-        // [HttpGet("{id}")]
-        // [ProducesResponseType(StatusCodes.Status200OK)]
-       // [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
-       // public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
-        //  public async Task<ActionResult<string>> GetProduct(int id)
-        // {
-        //     // var spec = new ProductsWithTypesAndBrandsSpecification(id);
+      //  // [Cached(600)]
+      //   [HttpGet("{id}")]
+      //   //[ProducesResponseType(StatusCodes.Status200OK)]
+      // // [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+      //  public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+      //    public async Task<ActionResult<string>> GetProduct(int id)
+      //   {
+      //       // var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
-        //     // var product = await _productsRepo.GetEntityWithSpec(spec);
+      //       // var product = await _productsRepo.GetEntityWithSpec(spec);
 
-        //     // if (product == null) return NotFound(new ApiResponse(404));
+      //       // if (product == null) return NotFound(new ApiResponse(404));
 
-        //     // return _mapper.Map<Product, ProductToReturnDto>(product);
-        //     return "unk";
-        // }
+      //       // return _mapper.Map<Product, ProductToReturnDto>(product);
+      //       return "unk";
+      //   }
         
     }
 }
--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastruture.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,7 +13,7 @@ builder.Services.AddDbContext<StoreContext>(opt=>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +26,25 @@ app.UseRouting();
 app.UseHttpsRedirection();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+// var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+// var userManager = services.GetRequiredService<UserManager<AppUser>>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    // await identityContext.Database.MigrateAsync();
+    await StorageContextSeed.SeedAsync(context);
+    // await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "An error occured during migration");
+}
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
